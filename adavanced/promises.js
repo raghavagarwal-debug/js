6756a45d5a1f251e6202fb86a9d1d34c19cf64cc
raghavagarwal-debug <raghavagarwal.cku12@gmail.com>
2026-05-21 18:27:29 +0000

    advanced

---
+++ adavanced/promises.js
@@ -0,0 +1,13 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    //DB calls, crytography, network
+    setTimeout(function()
+{
+    console.log('Async task is complete');
+}, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("promise consumed");
+    
+})
